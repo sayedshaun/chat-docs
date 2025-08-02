896fe64735e17f6e5d48cebfc030c8d40023bec9
sayedshaun <sayedshaun4@gmail.com>
2025-08-02 22:57:35 +0600

    Refactor agent.py: remove unused code and implement create_rag_agent function

--- src/agent.py
+++ src/agent.py
@@ -1,33 +1,24 @@
-# executor.py
+# agent.py
 import re
-from typing import Awaitable, Callable, List
-from langchain_community.agent_toolkits.sql.base import create_sql_agent
-from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit  
-from langchain.agents import AgentExecutor
-from langchain_core.runnables.base import Runnable
-from .prompt import (
-    DOCUMENT_EXECUTOR_PROMPT, 
-)
+from typing import Sequence
+from langchain.agents import initialize_agent, AgentExecutor, AgentType, create_react_agent
 from .provider import llm
-from .database import chroma
 from .utils import calculate_execution_time
-from .state import Message
+from langchain_core.language_models import LanguageModelLike
+from langchain_core.tools import BaseTool
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 @calculate_execution_time
-def document_executor() -> Callable[[str], Awaitable[str]]:
-    retriever = chroma.as_retriever(search_kwargs={"k": 3})
-    prompt = DOCUMENT_EXECUTOR_PROMPT
-
-    def retrieval_chain(query: str) -> str:
-        docs = retriever.invoke(query)
-        context = "\n".join(doc.page_content for doc in docs)
-        formatted_prompt = prompt.format(context=context, question=query)
-        response = llm.invoke(formatted_prompt)
-        return response.content
-    return retrieval_chain
-
-
-def router_executor(question: str) -> dict:
-    response = llm.invoke(question)
-    return response.content
-
+def create_rag_agent(
+    llm: LanguageModelLike, tools: Sequence[BaseTool], verbose: bool = False
+    ) -> AgentExecutor:
+    """Create a RAG agent for the RAG system."""
+    agent = initialize_agent(
+        tools=tools, llm=llm, verbose=verbose,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        handle_parsing_errors=True
+    )
+    return agent
