c01170c78fe10a7c80120629685944258e660c8b
sayedshaun <sayedshaun4@gmail.com>
2025-08-02 22:58:17 +0600

    Enhance loader.py: add support for additional document formats and refactor load_files_from_directory function

--- src/loader.py
+++ src/loader.py
@@ -1,34 +1,43 @@
 #loader.py
+import glob
 import os
+
+from pyparsing import Optional
 from .utils import calculate_execution_time
 from typing import List
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import (
-    UnstructuredPDFLoader,
     TextLoader, 
     UnstructuredWordDocumentLoader,
+    UnstructuredMarkdownLoader,
+    UnstructuredHTMLLoader,
+    UnstructuredCSVLoader,
+    UnstructuredPowerPointLoader,
+    UnstructuredExcelLoader,
+    UnstructuredXMLLoader,
+    UnstructuredEmailLoader,
+    JSONLoader,
+    PyPDFLoader,
+    WebBaseLoader,
+    WikipediaLoader,
 )
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+
+load_dotenv()
 
 @calculate_execution_time
 def load_files_from_directory(directory: str) -> List:
-    """
-    Load all documents from a specified directory.
-    Supports PDF files and other text-based formats.
-    """
+    """Load all documents from the directory"""
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=os.getenv("CHUNK_SIZE"), 
-        chunk_overlap=os.getenv("CHUNK_OVERLAP")
+        chunk_size=int(os.getenv("CHUNK_SIZE")), 
+        chunk_overlap=int(os.getenv("CHUNK_OVERLAP"))
     )
     pdf_docs = splitter.split_documents(
         DirectoryLoader(
             path=directory,
             glob="**/*.pdf",
-            loader_cls=UnstructuredPDFLoader,
-            loader_kwargs={
-                "mode": "elements",
-                "language": "ben",
-            }
+            loader_cls=PyPDFLoader,
         ).load()
     )
     text_docs = splitter.split_documents(
@@ -45,33 +54,72 @@ def load_files_from_directory(directory: str) -> List:
             loader_cls=UnstructuredWordDocumentLoader
         ).load()
     )
-    splited_documents = pdf_docs + text_docs + word_docs
+    markdown_docs = splitter.split_documents(
+        DirectoryLoader(
+            path=directory,
+            glob="**/*.md",
+            loader_cls=UnstructuredMarkdownLoader
+        ).load()
+    )
+    html_docs = splitter.split_documents(
+        DirectoryLoader(
+            path=directory,
+            glob="**/*.html",
+            loader_cls=UnstructuredHTMLLoader
+        ).load()
+    )
+    ppt_docs = splitter.split_documents(
+        DirectoryLoader(
+            path=directory,
+            glob="**/*.pptx",
+            loader_cls=UnstructuredPowerPointLoader
+        ).load()
+    )
+    excel_docs = splitter.split_documents(
+        DirectoryLoader(
+            path=directory,
+            glob="**/*.xlsx",
+            loader_cls=UnstructuredExcelLoader
+        ).load()
+    )
+    xml_docs = splitter.split_documents(
+        DirectoryLoader(
+            path=directory,
+            glob="**/*.xml",
+            loader_cls=UnstructuredXMLLoader
+        ).load()
+    )
+    email_docs = splitter.split_documents(
+        DirectoryLoader(
+            path=directory,
+            glob="**/*.eml",
+            loader_cls=UnstructuredEmailLoader
+        ).load()
+    )
+    json_docs = splitter.split_documents(
+        DirectoryLoader(
+            path=directory,
+            glob="**/*.json",
+            loader_cls=JSONLoader
+        ).load()
+    )
+    splited_documents = (
+        pdf_docs + 
+        text_docs + 
+        word_docs +
+        markdown_docs +
+        html_docs +
+        ppt_docs +
+        excel_docs +
+        xml_docs +
+        email_docs +
+        json_docs
+    )
     return splited_documents
 
 
 @calculate_execution_time
-def load_file(file_path: str) -> List:
-    """
-    Load a single file based on its extension.
-    Supports PDF, TXT, and DOCX formats.
-    """
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500, chunk_overlap=50
-    )
-    if file_path.endswith(".pdf"):
-        return splitter.split_documents(
-            UnstructuredPDFLoader(file_path).load()
-        )
-    elif file_path.endswith(".txt"):
-        return splitter.split_documents(
-            TextLoader(file_path).load()
-        )
-    elif file_path.endswith(".docx"):
-        return splitter.split_documents(
-            UnstructuredWordDocumentLoader(file_path).load()
-        )
-    else:
-        raise ValueError(
-            "Unsupported file format. " \
-            "Only PDF, TXT, and DOCX files are supported."
-        )
+def load_csv_from_directory(directory: str) -> List:
+    """Load all documents from the directory"""
+    return glob.glob(f"{directory}/*.csv")
+
