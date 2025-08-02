cc050e096a8f4667d7634c71a70e2210cb778af4
sayedshaun <sayedshaun4@gmail.com>
2025-08-02 22:58:12 +0600

    Refactor prompt.py: remove unused prompt functions and implement create_agent_prompt for RAG agent

--- src/prompt.py
+++ src/prompt.py
@@ -1,4 +1,5 @@
 # prompts.py
+from typing import Any, Dict
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from functools import lru_cache
@@ -10,43 +11,39 @@ from langchain.prompts.chat import (
 )
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
 
-def document_executor_prompt() -> PromptTemplate:
-    template="""Answer the question based only on the following context:
 
-    Context: {context}
-    
-    Question: {question}
-    
-    If you don't find the answer in the context, say "I don't know"
-    """
-    return PromptTemplate(
-        input_variables=["context", "question"],
-        template=template
-    )
 
-@lru_cache(maxsize=1)
-def general_prompt() -> ChatPromptTemplate:
-    template = """Your are a helpful assistant that answers user's questions.
-    You are designed to do conversion with users and provide accurate information.
-    if user asks about any specific topic, you should provide detailed information from the context.
-    otherwise, you should provide general information. If user ask any question that is not related to the context, you should say "I don't know"
-    """
+def create_agent_prompt(input: str) -> Dict[str, Any]:
+    """Create a prompt for the RAG agent."""
 
-    system_message = SystemMessagePromptTemplate.from_template(template)    
-    example_human = HumanMessagePromptTemplate.from_template("Hi, what can you do?")
-    example_ai = AIMessagePromptTemplate.from_template("I can answer Gigatech HR questions!")
-    human_message = HumanMessagePromptTemplate.from_template("{question}")
+    RAG_AGENT_SYSTEM_TEMPLATE = """
+        You are a helpful AI assistant that answers user questions. You have access few tools.
 
-    prompt = ChatPromptTemplate.from_messages(
-        messages=[
-            system_message,
-            example_human,
-            example_ai,
-            human_message,
+        Guidelines:
+            - If you know the answer, provide it directly without using tools.
+            - If user ask general information, provide a concise and accurate response.
+            - If you want to use a tool then think and pick the right tool.
+            - If you need more information, use the tools to find relevant context.
+            - Always provide citations for any specific claims made.
+
+        Tools:
+            - `context_retriever_tool`: Use this tool to retrieve relevant context from the database.
+            - `sql_query_tool`: Use this tool to execute SQL queries and return results.
+            
+        When using `context_retriever_tool`:
+            - Use it to retrieve relevant context from the database.
+            - Check synonyms and related terms to ensure comprehensive retrieval.
+            - Use the retrieved context to answer questions or provide information.
+
+        When using `sql_query_tool`:
+            - First check if the query is valid SQL.
+            - Fetch table information to run accurate queries.
+            - Use it to execute SQL queries and return results.
+        """
+
+    return {
+        "messages": [
+            SystemMessage(content=RAG_AGENT_SYSTEM_TEMPLATE),
+            HumanMessage(content=input)
         ]
-    )
-    return prompt
-
-
-DOCUMENT_EXECUTOR_PROMPT = document_executor_prompt()
-GENERAL_PROMPT = general_prompt
+    }
