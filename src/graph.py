685903399123de07e5b0e83a6d1844f67732b14d
sayedshaun <sayedshaun4@gmail.com>
2025-08-02 22:58:30 +0600

    Refactor graph.py: replace document executor with RAG agent integration and streamline workflow creation

--- src/graph.py
+++ src/graph.py
@@ -1,46 +1,29 @@
-# workflow.py
-from functools import lru_cache
+# graph.py
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.state import CompiledStateGraph
-from langgraph.checkpoint.memory import MemorySaver
-from .state import GraphState
-from .agent import document_executor, router_executor
-from .utils import calculate_execution_time
-import logging
-from langchain_core.messages import AIMessage
-from typing import Dict, Any, Union
-
-logging.basicConfig(
-    level=logging.INFO, filename='info.log', 
-    filemode="a", format='[%(levelname)s] %(message)s'
-)
+from langgraph.checkpoint.memory import InMemorySaver
+from .state import AgentState
+from typing import Dict, Any
+from .tools import TOOLS
+from .provider import llm
+from .agent import create_rag_agent 
 
 
-def router_executor_node(state: GraphState) -> Dict[str, Union[str, AIMessage]]:
-    question = state["question"]
-    logging.info(f"question: {question}")
-    response = router_executor(state)
-    state["answer"] = response["answer"]
-    return {"answer": response["answer"]}
-
-def document_executor_node(state: GraphState) -> Dict[str, Union[str, AIMessage]]:        
-    doc_chain = document_executor()
-    question = state["question"]
-    logging.info(f"question: {question}")
-    answer = doc_chain(question)
-    state["answer"] = answer    
-    return {"answer": answer}
+def rag_agent_node(state: AgentState) -> Dict[str, Any]:
+    """Node that invokes the RAG agent to process the state."""
+    message = state["messages"]
+    agent = create_rag_agent(llm=llm, tools=TOOLS, verbose=True)
+    response = agent.invoke(message)
+    return {"messages": [response["output"]]}
 
 
-@calculate_execution_time
-def build_graph() -> CompiledStateGraph[GraphState]:
-    graph = StateGraph(GraphState)
-    graph.add_node("document_executor_node", document_executor_node)
-    graph.add_edge(START, "document_executor_node")
-    graph.add_edge("document_executor_node", END)
-    compiled_graph = graph.compile(checkpointer=MemorySaver())
+def create_workflow() -> CompiledStateGraph:
+    """ Create the workflow for the RAG agent."""
+    checkpointer = InMemorySaver()
+    graph = StateGraph(AgentState)
+    graph.add_node("rag_agent", rag_agent_node)
+    graph.add_edge(START, "rag_agent")
+    graph.add_edge("rag_agent", END) 
+    compiled_graph = graph.compile(checkpointer=checkpointer)
     return compiled_graph
-
-
-
 
