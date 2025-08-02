791c460a1e50ba99f2195d518be67c5196edd005
sayedshaun <sayedshaun4@gmail.com>
2025-08-02 22:58:36 +0600

    Refactor database.py: rename session functions for clarity and update persist directory to use environment variable

--- src/database.py
+++ src/database.py
@@ -2,19 +2,28 @@ import os
 from functools import lru_cache
 from dotenv import load_dotenv
 from langchain_chroma import Chroma
+from langchain_community.utilities import SQLDatabase
+from sqlalchemy import create_engine
 from .provider import embedding
 os.environ["CHROMA_TELEMETRY_ENABLED"] = "false"
 
 load_dotenv()
 
 @lru_cache(maxsize=1)
-def start_session() -> Chroma:
+def start_vectorstore_session() -> Chroma:
     return Chroma(
         collection_name="rag_collection",
         embedding_function=embedding,
-        persist_directory="./.chromadb"
+        persist_directory=os.getenv("VECTOR_DATABASE_URL")
     )
 
-chroma = start_session()
+@lru_cache(maxsize=1)
+def start_sql_database_session() -> SQLDatabase:
+    engine = create_engine(os.getenv("SQL_DATABASE_URL"))
+    return SQLDatabase(engine=engine)
 
-__all__ = ["chroma"]
+
+vectorstore = start_vectorstore_session()
+sqldatabase = start_sql_database_session()
+
+__all__ = ["vectorstore", "sqldatabase"]
