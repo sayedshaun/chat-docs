a486eab00183a0ae5c984b72146d00203f71ac0e
sayedshaun <sayedshaun4@gmail.com>
2025-08-05 15:35:24 +0600

    fixed and processed agent response

--- src/backend/router.py
+++ src/backend/router.py
@@ -1,3 +1,4 @@
+# router.py
 import asyncio
 from typing import AsyncGenerator, List
 from src.rag.ingest import update_database
@@ -5,11 +6,11 @@ from src.rag.graph import create_graph
 from src.rag.prompt import create_agent_prompt
 from fastapi import APIRouter, File, UploadFile
 from fastapi.responses import StreamingResponse, JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
+from langchain_core.messages import BaseMessage, AIMessage
 import shutil
 import os
 import tempfile
-from src.backend.schema import LLMResponse, Question
+from src.backend.schema import Message
 from dotenv import load_dotenv
 import logging
 
@@ -24,32 +25,34 @@ load_dotenv()
 router = APIRouter()
 config={"configurable":{"thread_id":1}}
 
-@router.get("/health")
-async def health_check():
-    return {"status": "ok"}
 
-@router.post("/ask", response_model=LLMResponse)
-async def ask_question(question: Question) -> LLMResponse:
+@router.post("/ask", response_model=Message)
+async def ask_question(message: Message) -> Message:
     graph = create_graph()
-    input_prompt = create_agent_prompt(question.question)
+    input_prompt = create_agent_prompt(message)
     response = graph.invoke(input_prompt, config=config)
-    logging.info(f"Question: {question.question}, Response: {response}")
-    return LLMResponse(answer=response["messages"][-1].content)
+    messages: List[BaseMessage] = response.get("messages", [])
+    last_ai_msg = next((m for m in reversed(messages) if isinstance(m, AIMessage)), None)
+    if not last_ai_msg:
+        return Message(content="Sorry, I couldn't generate a response.")
+    return Message(content=last_ai_msg.content)
 
-@router.post("/ask_stream")
-async def ask_question_stream(question: Question) -> StreamingResponse:
+
+@router.post("/ask_stream", response_class=StreamingResponse)
+async def ask_question_stream(question: Message) -> StreamingResponse:
     graph = create_graph()
-    input_prompt = create_agent_prompt(question.question)
     async def token_stream() -> AsyncGenerator[str, None]:
         for msg, metadata in graph.stream(
-            input_prompt, 
+            {"messages": question.content}, 
             stream_mode="messages", 
             config=config
             ):
-            if msg.content:
-                yield f"data: {msg.content}\n\n"
-                await asyncio.sleep(0)
+            if metadata["langgraph_node"]=="rag_agent_node":
+                if msg.content:
+                    yield f"data: {msg.content}\n\n"
+                    await asyncio.sleep(0)
     return StreamingResponse(token_stream(), media_type="text/event-stream")
+
 
 @router.post("/upload_files/")
 async def upload_files(files: List[UploadFile] = File(...)):
@@ -64,6 +67,7 @@ async def upload_files(files: List[UploadFile] = File(...)):
         return JSONResponse(content={"status": "Upload Success"})
     except Exception as e:
         raise RuntimeError(f"Failed to upload files: {str(e)}")
+
 
 @router.post("/update_database/")
 async def update_db():
