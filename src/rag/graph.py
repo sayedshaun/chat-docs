60e3f4916575d799a801ba34b71af725f4a5eac2
sayedshaun <sayedshaun4@gmail.com>
2025-08-05 15:35:53 +0600

    fixed node response

--- src/rag/graph.py
+++ src/rag/graph.py
@@ -2,6 +2,7 @@
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.state import CompiledStateGraph
 from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.messages import AIMessage
 from .state import AgentState
 from typing import Dict, Any
 from .tools import TOOLS
@@ -12,18 +13,19 @@ from .agent import create_rag_agent
 def rag_agent_node(state: AgentState) -> Dict[str, Any]:
     """Node that invokes the RAG agent to process the state."""
     message = state["messages"]
-    agent = create_rag_agent(llm=llm, tools=TOOLS, verbose=True)
+    agent = create_rag_agent(llm=llm, tools=TOOLS, verbose=False)
     response = agent.invoke(message)
-    return {"messages": [response["output"]]}
+    message = AIMessage(content=response["output"])
+    return {"messages": [message]}
 
 
-def create_graph() -> CompiledStateGraph:
+def create_graph() -> CompiledStateGraph[AgentState]:
     """ Create the workflow for the RAG agent."""
     checkpointer = InMemorySaver()
     graph = StateGraph(AgentState)
-    graph.add_node("rag_agent", rag_agent_node)
-    graph.add_edge(START, "rag_agent")
-    graph.add_edge("rag_agent", END) 
+    graph.add_node("rag_agent_node", rag_agent_node)
+    graph.add_edge(START, "rag_agent_node")
+    graph.add_edge("rag_agent_node", END) 
     compiled_graph = graph.compile(checkpointer=checkpointer)
     return compiled_graph
 
