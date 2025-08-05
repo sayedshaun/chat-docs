c75c211c0535b9c9d3f48611098b2f2d5a4df2f8
sayedshaun <sayedshaun4@gmail.com>
2025-08-05 15:36:06 +0600

    fixed prompt

--- src/rag/prompt.py
+++ src/rag/prompt.py
@@ -1,20 +1,8 @@
-# prompts.py
-from typing import Any, Dict
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
-from functools import lru_cache
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-    AIMessagePromptTemplate,
-)
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from langchain_core.messages import SystemMessage, HumanMessage, BaseMessage
+from typing import Dict, Any
 
-
-
-def create_agent_prompt(input: str) -> Dict[str, Any]:
-    """Create a prompt for the RAG agent."""
+def create_agent_prompt(message: BaseMessage) -> Dict[str, Any]:
+    """Create a valid AgentState input with a system and human message."""
 
     RAG_AGENT_SYSTEM_TEMPLATE = """
         You are a helpful AI assistant that answers user questions. You have access few tools.
@@ -27,23 +15,23 @@ def create_agent_prompt(input: str) -> Dict[str, Any]:
             - Always provide citations for any specific claims made.
 
         Tools:
-            - `context_retriever_tool`: Use this tool to retrieve relevant context from the database.
-            - `sql_query_tool`: Use this tool to execute SQL queries and return results.
+            - context_retriever_tool: Use this tool to retrieve relevant context from the database.
+            - sql_query_tool: Use this tool to execute SQL queries and return results.
             
-        When using `context_retriever_tool`:
+        When using context_retriever_tool:
             - Use it to retrieve relevant context from the database.
             - Check synonyms and related terms to ensure comprehensive retrieval.
             - Use the retrieved context to answer questions or provide information.
 
-        When using `sql_query_tool`:
+        When using sql_query_tool:
             - First check if the query is valid SQL.
             - Fetch table information to run accurate queries.
             - Use it to execute SQL queries and return results.
-        """
+    """
 
     return {
         "messages": [
             SystemMessage(content=RAG_AGENT_SYSTEM_TEMPLATE),
-            HumanMessage(content=input)
+            HumanMessage(content=message.content)
         ]
     }
