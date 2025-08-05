b5667aad200671320047c7406944e2d908236cef
sayedshaun <sayedshaun4@gmail.com>
2025-08-05 15:36:20 +0600

    fixed state bug

--- src/rag/state.py
+++ src/rag/state.py
@@ -1,6 +1,7 @@
 # state.py
 from typing import TypedDict, List, Annotated
-from langgraph.graph.message import BaseMessage, add_messages
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage
 
 
 class AgentState(TypedDict, total=False):
